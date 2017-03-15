332d9cc1f4dd44be769dcaa9fd387876acf4258c
Vittorio <you@example.com>
2017-03-15 19:24:47 +0100

    hola

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+function test(){
+	//alert("Hola!");
+    
+    for (i = 0; i < 5; i++) { 
+        document.getElementById('texto1').innerHTML += "<p class='text_red'>Soy el nuevo texto</p>";
+    }
+  
+}
